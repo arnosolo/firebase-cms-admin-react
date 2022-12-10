0f4252694b4db15f1edcd747ca571176fd7b319f
arno-solo <arno756@outlook.com>
2022-12-10 09:43:04 +0800

    change NoMatch text

--- src/components/NoMatch.tsx
+++ src/components/NoMatch.tsx
@@ -2,7 +2,7 @@ export function NoMatch () {
   return (
     <div className="flex flex-col gap-1 py-4 px-6 items-center text-gray-600">
       <i className="pi pi-info-circle text-7" />
-      <p className="text-lg font-sans">Page not found</p>
+      <p className="text-lg font-sans">No found</p>
     </div>
   )
 }
